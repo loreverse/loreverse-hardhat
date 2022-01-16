07a71924a036cf90e8b2aa56d4b1570b5e8875ce
nawoo <now.and.nawoo@gmail.com>
2022-01-16 14:27:00 +0900

    add test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,40 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Loreverse", function () {
+	let contract, contractWithAlice, contractWithBob;
+	let Owner, Alice, Bob;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+	beforeEach(async function () {
+		const factory = await ethers.getContractFactory("Loreverse");
+		contract = await factory.deploy();
+		await contract.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+		[Owner, Alice, Bob] = await ethers.getSigners();
+		contractWithAlice = contract.connect(Alice);
+		contractWithBob = contract.connect(Bob);
+	});
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+	it("name&symbol", async function () {
+		expect(await contract.name()).to.equal("Loreverse");
+		expect(await contract.symbol()).to.equal("LORE");
+	});
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+	it("metadata", async function () {
+		const tx1 = await contractWithAlice.mint("metadata1", "title1", "body1", 0); // tokenId = 1
+		await tx1.wait();
+		const tx2 = await contractWithBob.mint("metadata2", "title2", "body2", 1); // tokenId = 2
+		await tx2.wait();
+		expect(await contractWithAlice.tokenURI(1)).to.equal("metadata1");
+		expect(await contractWithAlice.tokenURI(2)).to.equal("metadata2");
+	});
+
+	it("getForkCount", async function () {
+		const tx1 = await contractWithAlice.mint("metadata1", "title1", "body1", 0); // tokenId = 1
+		await tx1.wait();
+		const tx2 = await contractWithBob.mint("metadata2", "title2", "body2", 1); // tokenId = 2
+		await tx2.wait();
+		expect(await contract.getForkCount(1)).to.equal(1);
+		expect(await contract.getForkCount(2)).to.equal(0);
+	});
 });
