839b82fa71e82ade1e5ebe1544aa5d9f4c5973ea
nawoo <now.and.nawoo@gmail.com>
2022-01-16 19:29:23 +0900

    add test

--- test/sample-test.js
+++ test/sample-test.js
@@ -37,4 +37,31 @@ describe("Loreverse", function () {
 		expect(await contract.getForkCount(1)).to.equal(1);
 		expect(await contract.getForkCount(2)).to.equal(0);
 	});
+
+	it("getForkCount2", async function () {
+		// tx.waitはなくてもいい?
+		await contractWithAlice.mint("metadata1", "title1", "body1", 0); // tokenId = 1
+		await contractWithAlice.mint("metadata2", "title2", "body2", 1); // tokenId = 2
+		await contractWithBob.mint("metadata3", "title3", "body3", 1); // tokenId = 3
+		await contractWithBob.mint("metadata4", "title4", "body4", 2); // tokenId = 4
+		expect(await contract.getForkCount(1)).to.equal(2);
+		expect(await contract.getForkCount(2)).to.equal(1);
+		expect(await contract.getForkCount(3)).to.equal(0);
+		expect(await contract.getForkCount(4)).to.equal(0);
+	});
+
+	it("totalSupply", async function () {
+		await contractWithAlice.mint("metadata1", "title1", "body1", 0); // tokenId = 1
+		await contractWithAlice.mint("metadata2", "title2", "body2", 1); // tokenId = 2
+		await contractWithBob.mint("metadata3", "title3", "body3", 1); // tokenId = 3
+		await contractWithBob.mint("metadata4", "title4", "body4", 2); // tokenId = 4
+		expect(await contract.totalSupply()).equal(4);
+	});
+
+	it("nonexistent token", async function () {
+		await expect(contract.tokenURI(0)).revertedWith("ERC721Metadata: URI query for nonexistent token");
+		await expect(contract.tokenURI(1)).revertedWith("ERC721Metadata: URI query for nonexistent token");
+		await expect(contract.getForkCount(0)).revertedWith("nonexistent token");
+		await expect(contract.getForkCount(1)).revertedWith("nonexistent token");
+	});
 });
